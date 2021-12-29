03ac1b4acd2b11f5a34198c2ea8a55ea808e9202
Vladimir Solovyov <leilyngale@icloud.com>
2021-12-29 11:39:18 +0800

    Simple buttons proj

--- course1/web_buttons/script.js
+++ course1/web_buttons/script.js
@@ -1,7 +1,29 @@
-function sayHi(){
-  var message = "Are you sure you want to cancel? Sure sure???";
-  if (confirm('Hi there! a new popup!')){
-    message = "You pressed OK!";
+function changeColor(){
+  var confirmMessage = "Do you want to change the first div to blue and the second div to yellow?";
+  var message = "Ok, no change this time!";
+  if (confirm(confirmMessage)){
+    var dd1 = document.getElementById('div1');
+    var dd2 = document.getElementById('div2');
+
+    dd1.className = "blueback";
+    dd2.className = "yellowback";
+    message = "Ok, the divs colors changed!";
+  }
+  alert(message);
+}
+
+function changeText(){
+  var confirmMessage = "Do you want to change text of the first div to 'Bonjour' and of the second div to 'Sayonara'?";
+  var message = "Ok, no change this time!";
+  if (confirm(confirmMessage)){
+    var dd1 = document.getElementById('div1');
+    var dd2 = document.getElementById('div2');
+
+    dd1.innerHTML = "Bonjour";
+    dd1.style.color = "white";
+    dd2.innerHTML = "Sayonara";
+    dd2.style.color = "blue";
+    message = "Ok, the divs text changed!";
   }
   alert(message);
 }

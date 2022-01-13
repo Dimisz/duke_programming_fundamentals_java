caeaca543a68a163f40692e84b5effaf653f0606
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-14 06:45:04 +0800

    Finding out of range errors

---
+++ course2/StringsFirstAssignment/src/DebuggingPart1.java
@@ -0,0 +1,25 @@
+public class DebuggingPart1 {
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void findAbc(String input) {
+        int index = input.indexOf("abc");
+        while (true) {
+            if (index == -1) {
+                break;
+            }
+            //if(index > input.length()-3) break;
+            System.out.println((index+1));
+            System.out.println((index+4));
+            String found = input.substring(index+1, index+4);
+            System.out.println(found);
+            index = input.indexOf("abc", index+4);
+        }
+    }
+    public static void test() {
+        //no code yet
+        //findAbc("abcd");
+        findAbc("abcbbbabcdddabc");
+    }
+}

b29e900b3989c6456959e9e19fd4fdd4a95752d3
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-28 17:24:05 +0800

    Starting ArrayLists

---
+++ course3/week2/TellingRandomStory/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WordFrequencies wordFreqs = new WordFrequencies();
+        wordFreqs.tester();
+//        WordsWithArrays wordsWithArrays = new WordsWithArrays();
+//        wordsWithArrays.tester();
+    }
+}

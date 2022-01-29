e205d67815d7781d4f6e63633deedde4db51215c
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-29 11:09:06 +0800

    Glad libs project

---
+++ course3/week2/GladLibs/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GladLib gladLib = new GladLib();
+        gladLib.makeStory();
+//        ArrayList<String> arr = new ArrayList<String>();
+//        arr.add("Hi");
+//        arr.add("MMy");
+//        System.out.println(arr.indexOf("Hi"));
+//        System.out.println(arr.indexOf("MMy"));
+//        System.out.println(arr.indexOf("ddd"));
+    }
+}

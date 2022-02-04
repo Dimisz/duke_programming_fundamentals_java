c9933f60fee2b2169d44cbdd64cafb8174bd27d6
Vladimir Solovyov <leilyngale@icloud.com>
2022-02-04 16:51:05 +0800

    Vigenere part2 completed for unknown key

--- course3/week4/VigenereCipher/src/VigenereBreaker.java
+++ course3/week4/VigenereCipher/src/VigenereBreaker.java
@@ -21,13 +21,68 @@ public class VigenereBreaker {
         return key;
     }
 
-    public void breakVigenere () {
+    public void breakVigenereWithKey (int keyLength, char mostCommon) {
         FileResource fr = new FileResource();
         String text = fr.asString();
-        int[] key = tryKeyLength(text, 5, 'e');
+        int[] key = tryKeyLength(text, keyLength, mostCommon);
+        for(int i : key) {
+            System.out.print(i + " ");
+        }
         VigenereCipher vc = new VigenereCipher(key);
         String message = vc.decrypt(text);
         System.out.println(message);
+    }
+
+    //====Part 2 of the week===================
+    public HashSet<String> readDictionary(){
+        FileResource fr = new FileResource();
+        HashSet<String> dictionary = new HashSet<String>();
+        for(String word : fr.lines()){
+            dictionary.add(word.toLowerCase());
+        }
+        return dictionary;
+    }
+
+    public int countWords(String message, HashSet<String> dictionary){
+        int count = 0;
+        for(String word : message.split("\\W+")){
+            if(dictionary.contains(word.toLowerCase())){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String breakForLanguage(String encrypted, HashSet<String> dict){
+        String mostRealDecryption = "Not decrypted yet";
+        int maxWords = 0;
+        int usedKLength = -1;
+        for(int i = 1; i < 101; i++){
+            int[] key = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher vc = new VigenereCipher(key);
+            String decrypted = vc.decrypt(encrypted);
+            int wordCounts = countWords(decrypted, dict);
+            if(wordCounts > maxWords){
+                maxWords = wordCounts;
+                mostRealDecryption = decrypted;
+                usedKLength = i;
+            }
+            if(i == 38){
+                System.out.println("Key: " + i);
+                System.out.println("Valid words: " + wordCounts);
+            }
+        }
+        System.out.println("Valid words: " + maxWords);
+        System.out.println("KLength used: " + usedKLength);
+        return mostRealDecryption;
+    }
+
+    public void breakVigenereWithoutKey() {
+        HashSet<String> dict = readDictionary();
+        FileResource fr = new FileResource();
+        String text = fr.asString();
+        String result = breakForLanguage(text, dict);
+        System.out.println(result);
     }
 
 
@@ -80,9 +135,15 @@ public class VigenereBreaker {
         System.out.println("ej :\t" + slice);
     }
 
-    public static void testBreakVigenere(){
+    public static void testBreakVigenereWithKey(){
         VigenereBreaker vb = new VigenereBreaker();
-        vb.breakVigenere();
+        vb.breakVigenereWithKey(4, 'e');
+    }
+
+
+    public static void testBreakVigenereWithoutKey(){
+        VigenereBreaker vb = new VigenereBreaker();
+        vb.breakVigenereWithoutKey();
     }
 
     //=============================================
@@ -90,7 +151,8 @@ public class VigenereBreaker {
     public static void main(String[] args) {
 //        testSliceString();
 //        testTryKeyLength();
-        testBreakVigenere();
+//        testBreakVigenereWithKey();
+        testBreakVigenereWithoutKey();
     }
     
 }

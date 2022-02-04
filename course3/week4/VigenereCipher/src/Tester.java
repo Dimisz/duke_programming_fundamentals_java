316ff9fb9b45909037c4c598f1441db9f56d7f1b
Vladimir Solovyov <leilyngale@icloud.com>
2022-02-04 09:47:06 +0800

    Startinng the Breaking Vigenere cipher project

---
+++ course3/week4/VigenereCipher/src/Tester.java
@@ -0,0 +1,42 @@
+import edu.duke.FileResource;
+
+public class Tester {
+
+    public static void testCaesarCipher(){
+        CaesarCipher cc = new CaesarCipher(5);
+        FileResource fr = new FileResource();
+        String text = fr.asString();
+        String encrypted = cc.encrypt(text);
+        System.out.println(encrypted);
+        String decrypted = cc.decrypt(encrypted);
+        System.out.println("\n" + decrypted);
+    }
+
+    public static void testCaesarCrackerEnglish(){
+        CaesarCracker cc = new CaesarCracker();
+        FileResource fr = new FileResource();
+        String text = fr.asString();
+        System.out.println(text);
+        int key = cc.getKey(text);
+        System.out.println("\nKey: " + key);
+        String decrypted = cc.decrypt(text);
+        System.out.println(decrypted);
+    }
+
+    public static void testCaesarCrackerNonEnglish(char commonChar){
+        CaesarCracker cc = new CaesarCracker(commonChar);
+        FileResource fr = new FileResource();
+        String text = fr.asString();
+        System.out.println(text);
+        int key = cc.getKey(text);
+        System.out.println("\nKey: " + key);
+        String decrypted = cc.decrypt(text);
+        System.out.println(decrypted);
+    }
+
+    public static void main(String[] args) {
+//        testCaesarCipher();
+//        testCaesarCrackerEnglish();
+        testCaesarCrackerNonEnglish('a');
+    }
+}

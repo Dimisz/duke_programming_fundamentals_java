14fbf10f8c168c48e9aae7d10fbec0f0f0f4bf64
Vladimir Solovyov <leilyngale@icloud.com>
2022-02-05 09:57:34 +0800

    Starting a project on analzing earthquakes

---
+++ course4/week1/EarthQuakeData/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    //PATH PREFIX
+    // /Users/mbpro/Downloads/SearchingEarthquakeDataStarterProgram
+    public static void main(String[] args) {
+        EarthQuakeClient eqClient = new EarthQuakeClient();
+//        eqClient.createCSV();
+        eqClient.bigQuakes();
+    }
+}

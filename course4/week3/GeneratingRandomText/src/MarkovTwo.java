0ff1842b268d461aaeb72047cab19b561e204620
Vladimir Solovyov <leilyngale@icloud.com>
2022-02-12 16:41:35 +0800

    Started a project on text generation with Markov chains

---
+++ course4/week3/GeneratingRandomText/src/MarkovTwo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class MarkovTwo {
+    private String myText;
+    private Random myRandom;
+
+    public MarkovTwo(){
+        myRandom = new Random();
+    }
+
+    public void setRandom(int seed){
+
+    }
+}

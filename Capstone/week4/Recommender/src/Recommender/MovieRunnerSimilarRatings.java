e1a0ab53d16bf21a812d9a65068c0050863a54ae
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-13 17:57:57 +0300

    Implemented multiple filters

--- Capstone/week4/Recommender/src/Recommender/MovieRunnerSimilarRatings.java
+++ Capstone/week4/Recommender/src/Recommender/MovieRunnerSimilarRatings.java
@@ -122,11 +122,66 @@ public class MovieRunnerSimilarRatings {
     }
 
 
+    public static void printSimilarRatingsByGenreAndMinutes(String genre, int minMinutes, int maxMinutes, String raterId, int numSimilarRaters, int minimalRaters){
+        String ratersFilename = "/Users/mbpro/Downloads/StepOneStarterProgram/data/ratings.csv";
+        String moviesFilename = "/Users/mbpro/Downloads/StepOneStarterProgram/data/ratedmoviesfull.csv";
+
+        FourthRatings fourthRatings = new FourthRatings();
+
+        MovieDatabase.initialize(moviesFilename);
+        System.out.println("Number of movies: " + MovieDatabase.size());
+
+        RaterDatabase.initialize(ratersFilename);
+        System.out.println("Number of ratings: " + RaterDatabase.size());
+
+        AllFilters allFilters = new AllFilters();
+        allFilters.addFilter(new GenreFilter(genre));
+        allFilters.addFilter(new MinutesFilter(minMinutes, maxMinutes));
+
+        ArrayList<Rating> similarRatings = fourthRatings.getSimilarRatingsByFilter(raterId, numSimilarRaters, minimalRaters, allFilters);
+        for(Rating rating : similarRatings){
+            String movieId = rating.getItem();
+            String movieTitle = MovieDatabase.getTitle(movieId);
+            System.out.println(movieTitle + " : " + rating.getValue());
+            //break;
+        }
+
+    }
+
+
+    public static void printSimilarRatingsByYearAfterAndMinutes(int year, int minMinutes, int maxMinutes, String raterId, int numSimilarRaters, int minimalRaters){
+        String ratersFilename = "/Users/mbpro/Downloads/StepOneStarterProgram/data/ratings.csv";
+        String moviesFilename = "/Users/mbpro/Downloads/StepOneStarterProgram/data/ratedmoviesfull.csv";
+
+        FourthRatings fourthRatings = new FourthRatings();
+
+        MovieDatabase.initialize(moviesFilename);
+        System.out.println("Number of movies: " + MovieDatabase.size());
+
+        RaterDatabase.initialize(ratersFilename);
+        System.out.println("Number of ratings: " + RaterDatabase.size());
+
+        AllFilters allFilters = new AllFilters();
+        allFilters.addFilter(new YearAfterFilter(year));
+        allFilters.addFilter(new MinutesFilter(minMinutes, maxMinutes));
+
+        ArrayList<Rating> similarRatings = fourthRatings.getSimilarRatingsByFilter(raterId, numSimilarRaters, minimalRaters, allFilters);
+        for(Rating rating : similarRatings){
+            String movieId = rating.getItem();
+            String movieTitle = MovieDatabase.getTitle(movieId);
+            System.out.println(movieTitle + " : " + rating.getValue());
+            //break;
+        }
+
+    }
+
+
     public static void main(String[] args) {
 //        printAverageRatings(5);
 //        printSimilarRatings("65", 20, 5);
 //        printSimilarRatingsByGenre("Action","65", 20, 5);
-        printSimilarRatingsByDirector("Clint Eastwood,Sydney Pollack,David Cronenberg,Oliver Stone","1034", 10, 3);
-
+//        printSimilarRatingsByDirector("Clint Eastwood,Sydney Pollack,David Cronenberg,Oliver Stone","1034", 10, 3);
+//        printSimilarRatingsByGenreAndMinutes("Adventure", 100, 200, "65", 10, 5);
+//        printSimilarRatingsByYearAfterAndMinutes(2000, 80, 100, "65",10, 5);
     }
 }

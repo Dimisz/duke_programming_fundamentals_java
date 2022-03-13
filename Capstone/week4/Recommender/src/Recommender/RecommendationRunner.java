bc03039dcce178ae5254441e12ac2f911284b6a4
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-13 19:04:47 +0300

    Preparing recommender for deployment

--- Capstone/week4/Recommender/src/Recommender/RecommendationRunner.java
+++ Capstone/week4/Recommender/src/Recommender/RecommendationRunner.java
@@ -3,13 +3,30 @@ package Recommender;
 import java.util.ArrayList;
 
 public class RecommendationRunner implements Recommender{
-    @Override
     public ArrayList<String> getItemsToRate() {
-        return null;
+        ArrayList<String> movies = MovieDatabase.filterBy(new TrueFilter());
+        ArrayList<String> moviesToRate = new ArrayList<String>();
+        for(int i = 0; i < 30; i ++) {
+            moviesToRate.add(movies.get(i));
+        }
+        return moviesToRate;
     }
 
-    @Override
     public void printRecommendationsFor(String webRaterID) {
-
+        FourthRatings fr = new FourthRatings();
+        //AllFilters f = new AllFilters();
+        //f.addFilter(new GenreFilter("Comedy"));
+        //f.addFilter(new YearAfterFilter(2000));
+        ArrayList<Rating> movies = fr.getSimilarRatingsByFilter(webRaterID, 50, 2, new TrueFilter());
+        if(movies.size() == 0) {
+            System.out.println("<p>Sorry, we were not able to find any recommendations for you.</p>");
+            System.exit(1);
+        }
+        System.out.println("<table>");
+        System.out.println("<tr><th>Rank</th><th>Movie Title</th></tr>");
+        for(int i = 0; i < 20; i ++) {
+            System.out.println("<tr><td>"+(i+1)+"</td><td>" + MovieDatabase.getTitle(movies.get(i).getItem()) + "</td></tr>");
+        }
+        System.out.println("</table>");
     }
 }

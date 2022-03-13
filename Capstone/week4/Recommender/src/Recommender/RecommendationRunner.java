ec7daf9dfb3f978d13b5dbf90033e98d2b86db41
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-13 18:52:13 +0300

    Started the final part

---
+++ Capstone/week4/Recommender/src/Recommender/RecommendationRunner.java
@@ -0,0 +1,15 @@
+package Recommender;
+
+import java.util.ArrayList;
+
+public class RecommendationRunner implements Recommender{
+    @Override
+    public ArrayList<String> getItemsToRate() {
+        return null;
+    }
+
+    @Override
+    public void printRecommendationsFor(String webRaterID) {
+
+    }
+}
